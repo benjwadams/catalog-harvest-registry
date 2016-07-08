26bf47c8ab0945063be91de2ed0e3a655054372e
Luke Campbell <luke.s.campbell@gmail.com>
2016-07-08 08:39:23 -0400

    Adds relative path support
    Now, when the application is deployed and the ROOT_URL environment
    variable is set, all of static content will render properly.

--- app/imports/ui/pages/banner.js
+++ app/imports/ui/pages/banner.js
@@ -1,1 +1,11 @@
 import './banner.html';
+
+import { Template } from 'meteor/templating';
+import { Meteor } from 'meteor/meteor';
+
+Template.App_banner.helpers({
+  absoluteUrl(path) {
+    return Meteor.absoluteUrl(path);
+  }
+});
+
